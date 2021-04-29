8daa0b72ea8ae07ded4bc357ac3e365a4b647377
Tobias Wallner <wallner3@hm.edu>
2021-04-29 13:29:29 +0200

    Display maps 100%

--- WebApp/tourguide/src/app/app-routing.module.ts
+++ WebApp/tourguide/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { MapHereComponent } from './components/map-here/map-here.component';
 import { MapMapboxComponent } from './components/map-mapbox/map-mapbox.component';
+import { TestComponent } from './components/test/test.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AppComponent,
+    component: TestComponent,
   },
   {
     path: 'here',
