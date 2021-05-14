34643ecf210a50ebaf617f79c93da57a43087a31
Christian Glasl <cglasl@hm.edu>
2021-05-14 12:28:04 +0200

    add pois

--- WebApp/tourguide/src/app/app-routing.module.ts
+++ WebApp/tourguide/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MapGoogleComponent } from './components/map-google/map-google.component';
 import { TestComponent } from './components/test/test.component';
+import { ViewPoisComponent } from './components/view-pois/view-pois.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,7 @@ const routes: Routes = [
     component: TestComponent,
   },
   { path: 'gmaps', component: MapGoogleComponent },
+  { path: 'pois', component: ViewPoisComponent },
 ];
 
 @NgModule({
