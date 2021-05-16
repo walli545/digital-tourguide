b72940df654d47a71c065dea29f88d79c52e9397
Marcel Andre Koehler <marcel.koehler@hm.edu>
2021-05-16 11:14:40 +0200

    Added missing termsOfService uri

--- Backend/API/src/IO.Swagger/Startup.cs
+++ Backend/API/src/IO.Swagger/Startup.cs
@@ -81,7 +81,7 @@ namespace IO.Swagger
                            Url = new Uri("https://github.com/swagger-api/swagger-codegen"),
                            Email = ""
                         },
-                        TermsOfService = new Uri("")
+                        TermsOfService = new Uri("http://swagger.io/terms/")
                     });
                     c.CustomSchemaIds(type => type.FullName);
                     c.IncludeXmlComments($"{AppContext.BaseDirectory}{Path.DirectorySeparatorChar}{_hostingEnv.ApplicationName}.xml");
