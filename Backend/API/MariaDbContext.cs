1ffd720b26eada34d549fd91f6a1c2ae7571b2ad
Marcel Andre Koehler <marcel.koehler@hm.edu>
2021-05-18 14:05:54 +0200

    PoI Endpoint implementation

---
+++ Backend/API/MariaDbContext.cs
@@ -0,0 +1,25 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    /// <summary>
+    /// Database context for a mariadb
+    /// </summary>
+    public partial class MariaDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+      /// <summary>
+      /// Ctor.
+      /// </summary>
+      /// <param name="options">DbContext options for the database</param>
+      public MariaDbContext(DbContextOptions<MariaDbContext> options)
+          : base(options)
+      {
+      }
+
+      /// <summary>
+      /// The DbSets of the poi's
+      /// </summary>
+      public virtual DbSet<PointOfInterest> PointOfInterest { get; set; }
+    }
+  }
