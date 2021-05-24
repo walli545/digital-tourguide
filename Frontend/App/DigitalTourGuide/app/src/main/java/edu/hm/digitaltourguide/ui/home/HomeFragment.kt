947154b9e373565a6a5e1a0e85b371cc96856aff
Sven Schötz <sven.schoetz@kabelmail.de>
2021-05-24 22:38:14 +0200

    Create demo route preview in home fragment

--- Frontend/App/DigitalTourGuide/app/src/main/java/edu/hm/digitaltourguide/ui/home/HomeFragment.kt
+++ Frontend/App/DigitalTourGuide/app/src/main/java/edu/hm/digitaltourguide/ui/home/HomeFragment.kt
@@ -8,20 +8,55 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import edu.hm.digitaltourguide.R
+import edu.hm.digitaltourguide.RoutePreviewAdapter
+import edu.hm.digitaltourguide.api.models.PointOfInterest
+import edu.hm.digitaltourguide.api.models.Route
+import java.math.BigDecimal
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var adapter: RoutePreviewAdapter
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val recyclerView: RecyclerView = root.findViewById(R.id.home_last_recycler)
+        adapter = RoutePreviewAdapter(
+            listOf(
+                Route(
+                    0,
+                    arrayOf(
+                        PointOfInterest(
+                            1,
+                            "Kirchsee",
+                            "",
+                            BigDecimal(47.8189),
+                            BigDecimal(11.6180),
+                            BigDecimal(0),
+                            0,
+
+                            )
+                    ),
+                    "TestTour",
+                    "",
+                    "",
+                    1.0f
+                )
+            )
+        )
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
         return root
     }
 }
