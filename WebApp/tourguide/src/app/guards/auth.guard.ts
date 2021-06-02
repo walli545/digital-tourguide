ba3912d2b49709bc3fc209749ddf0d2bc69131fd
Tobias Wallner <wallner3@hm.edu>
2021-06-02 22:27:09 +0200

    Website: Add first version of authguard

---
+++ WebApp/tourguide/src/app/guards/auth.guard.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { KeycloakAuthGuard, KeycloakService } from 'keycloak-angular';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard extends KeycloakAuthGuard {
+  constructor(
+    protected readonly router: Router,
+    protected readonly keycloak: KeycloakService
+  ) {
+    super(router, keycloak);
+  }
+
+  public async isAccessAllowed(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<UrlTree | boolean> {
+    // Force the user to log in if currently unauthenticated.
+    if (!this.authenticated) {
+      await this.keycloak.login({
+        redirectUri: window.location.origin + state.url,
+      });
+    }
+
+    if (!this.checkRolesEvery(route)) {
+      return false;
+    }
+    return this.checkRolesAny(route);
+  }
+
+  private checkRolesEvery(route: ActivatedRouteSnapshot): boolean {
+    const requiredRoles = route.data.everyRole;
+
+    if (!(requiredRoles instanceof Array) || requiredRoles.length === 0) {
+      return true;
+    }
+
+    return requiredRoles.every((role) => this.roles.includes(role));
+  }
+
+  private checkRolesAny(route: ActivatedRouteSnapshot): boolean {
+    const requiredRoles = route.data.anyRole;
+
+    if (!(requiredRoles instanceof Array) || requiredRoles.length === 0) {
+      return true;
+    }
+
+    return requiredRoles.some((role) => this.roles.includes(role));
+  }
+}
