58c98c49bc87555699f5d51cb6a75a66ba048dc3
Sven Schötz <sven.schoetz@kabelmail.de>
2021-06-02 14:49:22 +0200

    Fix app build and merge errors.

--- Frontend/App/DigitalTourGuide/app/src/main/java/edu/hm/digitaltourguide/api/infrastructure/Errors.kt
+++ Frontend/App/DigitalTourGuide/app/src/main/java/edu/hm/digitaltourguide/api/infrastructure/Errors.kt
@@ -3,38 +3,14 @@ package edu.hm.digitaltourguide.api.infrastructure
 
 import java.lang.RuntimeException
 
-open class ClientException : RuntimeException {
-
-    /**
-     * Constructs an [ClientException] with no detail message.
-     */
-    constructor() : super()
-
-    /**
-     * Constructs an [ClientException] with the specified detail message.
-
-     * @param   message   the detail message.
-     */
-    constructor(message: String) : super(message)
+open class ClientException(message: kotlin.String? = null, val statusCode: Int = -1, val response: Response? = null) : RuntimeException(message) {
 
     companion object {
         private const val serialVersionUID: Long = 123L
     }
 }
 
-open class ServerException : RuntimeException {
-
-    /**
-     * Constructs an [ServerException] with no detail message.
-     */
-    constructor() : super()
-
-    /**
-     * Constructs an [ServerException] with the specified detail message.
-
-     * @param   message   the detail message.
-     */
-    constructor(message: String) : super(message)
+open class ServerException(message: kotlin.String? = null, val statusCode: Int = -1, val response: Response? = null) : RuntimeException(message) {
 
     companion object {
         private const val serialVersionUID: Long = 456L
