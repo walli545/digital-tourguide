c46eaf5035781fc9b355c7aafe3b250e1d7f8b2d
Christian Glasl <cglasl@hm.edu>
2021-06-15 08:54:42 +0200

    Website: add role verify

---
+++ WebApp/tourguide/src/app/components/role-verify/role-verify.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-role-verify',
+  templateUrl: './role-verify.component.html',
+  styleUrls: ['./role-verify.component.scss']
+})
+export class RoleVerifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
