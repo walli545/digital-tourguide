eff3c6bd6a5ac0fa14601192f5ad4bf6beb443dc
TobiP3005 <65250269+TobiP3005@users.noreply.github.com>
2021-06-21 16:17:25 +0200

    demo data deleted

--- Frontend/App/DigitalTourGuide/app/src/main/java/edu/hm/digitaltourguide/ui/tour/TourDetailFragment.kt
+++ Frontend/App/DigitalTourGuide/app/src/main/java/edu/hm/digitaltourguide/ui/tour/TourDetailFragment.kt
@@ -25,13 +25,6 @@ class TourDetailFragment : Fragment() {
     private lateinit var binding: FragmentTourDetailBinding
     private lateinit var route: Route
 
-    // Demo POI Information
-    val poiTitleList = arrayOf("St. Marienkirche","Zionskirche","Kirche am Südstern","St. Johanniskirche")
-    val poiImageList = arrayOf("https://www.h-hotels.com/_Resources/Persistent/5a7e839511d272d52470d6f8cd1aed90527a691b/berlin-skyview-museumsinsel-01-2660x1990.jpg",
-        "https://www.h-hotels.com/_Resources/Persistent/3b16ed74d9a4b7a5bfb7cb1ca631bec93d49375f/berlin-nikolaikirche-01-2400x1351-1356x763.jpg",
-        "https://evkirche-luckenwalde.de/gra/johportal.jpg",
-        "https://upload.wikimedia.org/wikipedia/commons/b/bf/Johanneskirche_%28Berlin-Lichterfelde%29.jpg")
-
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
