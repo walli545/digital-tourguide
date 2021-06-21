b9d7d930ba141759e14fff23e2bf574d812c7d20
TobiP3005 <65250269+TobiP3005@users.noreply.github.com>
2021-06-22 01:33:10 +0200

    Tour bewerten (draft)

--- Frontend/App/DigitalTourGuide/app/src/main/java/edu/hm/digitaltourguide/ui/tour/TourDetailFragment.kt
+++ Frontend/App/DigitalTourGuide/app/src/main/java/edu/hm/digitaltourguide/ui/tour/TourDetailFragment.kt
@@ -4,19 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.RatingBar
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.denzcoskun.imageslider.models.SlideModel
 import edu.hm.digitaltourguide.BuildConfig
+import edu.hm.digitaltourguide.MainActivity
 import edu.hm.digitaltourguide.R
 import edu.hm.digitaltourguide.api.models.PointOfInterest
 import edu.hm.digitaltourguide.api.models.Route
 import edu.hm.digitaltourguide.databinding.FragmentTourDetailBinding
+import edu.hm.digitaltourguide.ui.poi.SwipeToDeleteCallback
 import edu.hm.digitaltourguide.ui.tourList.TourItemListener
 
 class TourDetailFragment : Fragment() {
@@ -44,6 +52,8 @@ class TourDetailFragment : Fragment() {
         val imageSlider = binding.tripImageSlider
         val poiList = binding.poiListRecycler
         val descriptionText = binding.tourDescription
+        val rating = binding.ratingBar
+        val rateBtn = binding.rateRouteBtn
 
         (requireActivity() as AppCompatActivity).supportActionBar?.title = route.name
 
@@ -71,8 +81,61 @@ class TourDetailFragment : Fragment() {
                 .into(binding.tourPreviewImage)
         }
 
+//        rating.rating = route.averageRating.toFloat()
+//
+//        val swipeDeleteHandler = object : SwipeToDeleteCallback(context) {
+//            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+//                AlertDialog.Builder(context!!)
+//                    .setTitle("Delete review")
+//                    .setMessage("Are you sure you want to delete this review?")
+//                    .setPositiveButton(
+//                        android.R.string.yes
+//                    ) { dialog, which ->
+//                        //                val item = ratingListAdapter.ratingList[viewHolder.adapterPosition]
+//                        //                API.deleteRating(item)
+//                        // Continue with delete operation
+//                    } // A null listener allows the button to dismiss the dialog and take no further action.
+//                    .setNegativeButton(android.R.string.no, null)
+//                    .setIcon(R.drawable.ic_delete_purple_24)
+//                    .show()
+//                ratingListAdapter.notifyDataSetChanged()
+//            }
+//        }
+//
+//        val role = MainActivity.preferences.getString("ROLE", null)
+//        if (role != null && role == "moderator"){
+//            val itemTouchHelperDelete = ItemTouchHelper(swipeDeleteHandler)
+//            itemTouchHelperDelete.attachToRecyclerView(ratingList)
+//        }
+//
+//        rateBtn.setOnClickListener{
+//            openAddPoiReviewDialog()
+//        }
 
         return binding.root
+    }
+
+    /**
+     * Method to add a tour review
+     */
+    private fun openAddTourReviewDialog(){
+        val d = AlertDialog.Builder(requireContext())
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.poi_dialog_rating, null)
+        d.setTitle("Tour bewerten")
+        d.setView(dialogView)
+        val ratingBar = dialogView.findViewById<RatingBar>(R.id.review_ratingBar)
+        val comment = dialogView.findViewById<EditText>(R.id.review_comment)
+
+        d.setPositiveButton("Done") { _, _ ->
+            // TODO
+            ratingBar.numStars.toDouble()
+            comment.text.toString()
+            Toast.makeText(requireContext(), "Bewertung erfolgreich abgegeben", Toast.LENGTH_LONG).show()
+        }
+        d.setNegativeButton("Cancel") { _, _ -> }
+        val alertDialog = d.create()
+        alertDialog.show()
     }
 }
 
