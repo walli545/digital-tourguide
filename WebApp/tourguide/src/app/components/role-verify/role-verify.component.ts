d800446a023642164e4a71d99252a9e813302ca1
Tobias Wallner <wallner3@hm.edu>
2021-06-24 15:02:11 +0200

    Website: Fix accepting of role request does not trigger an http request

--- WebApp/tourguide/src/app/components/role-verify/role-verify.component.ts
+++ WebApp/tourguide/src/app/components/role-verify/role-verify.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { RoleModel, VerificationService } from 'src/app/api';
 
 @Component({
@@ -9,9 +10,17 @@ import { RoleModel, VerificationService } from 'src/app/api';
 export class RoleVerifyComponent implements OnInit {
   requests = new Map<string, RoleModel>();
   loading = true;
-  constructor(private verificationService: VerificationService) {}
+  constructor(
+    private verificationService: VerificationService,
+    private snackBar: MatSnackBar
+  ) {}
 
   async ngOnInit(): Promise<void> {
+    this.loadRequests();
+  }
+
+  async loadRequests(): Promise<void> {
+    this.loading = true;
     try {
       const requests = await this.verificationService.getRequests().toPromise();
       for (const r of requests) {
@@ -22,13 +31,31 @@ export class RoleVerifyComponent implements OnInit {
     }
   }
 
-  onAccept(role: RoleModel): void {
-    this.verificationService.acceptRequest(role.creatorName);
-    this.requests.delete(role.creatorName);
+  async onAccept(role: RoleModel): Promise<void> {
+    this.loading = true;
+    try {
+      await this.verificationService
+        .acceptRequest(role.creatorName)
+        .toPromise();
+      this.requests.delete(role.creatorName);
+    } catch (e) {
+      this.snackBar.open('Failed to accept role request', undefined, {
+        duration: 3000,
+      });
+    }
+    this.loading = false;
   }
-  onDeny(role: RoleModel): void {
-    this.verificationService.denyRequest(role.creatorName);
-    this.requests.delete(role.creatorName);
+  async onDeny(role: RoleModel): Promise<void> {
+    this.loading = true;
+    try {
+      await this.verificationService.denyRequest(role.creatorName).toPromise();
+      this.requests.delete(role.creatorName);
+    } catch (e) {
+      this.snackBar.open('Failed to accept role request', undefined, {
+        duration: 3000,
+      });
+    }
+    this.loading = false;
   }
   toArray(): RoleModel[] {
     return Array.from(this.requests.values());
